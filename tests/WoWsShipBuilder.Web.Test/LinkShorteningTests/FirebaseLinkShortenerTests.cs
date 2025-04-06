ce5c24acadb2408ad679e6d0d54a9a31aab86e4b
Kontiky <torelli.stefano@gmail.com>
2025-04-06 21:30:05 +0200

    Update Packages Phase 3

--- tests/WoWsShipBuilder.Web.Test/LinkShorteningTests/FirebaseLinkShortenerTests.cs
+++ tests/WoWsShipBuilder.Web.Test/LinkShorteningTests/FirebaseLinkShortenerTests.cs
@@ -63,7 +63,6 @@ public class FirebaseLinkShortenerTests
     }
 
     [Test]
-    [Ignore("disabled due to issues during app deployment")]
     public async Task CreateShortLink_NormalLinkRateLimitExceeded_ShorteningDelayed()
     {
         const string testLink = "https://app.wowssb.com/charts";
